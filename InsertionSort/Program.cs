0f8c5cabe01edbd52da955f7ef4c1f2b1d7b033f
Thanh Vu <thanhvx@goquo.com>
2019-04-08 07:46:51 +0700

    update selection, insertion sort

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine ("Insertion Sort");
+            var random = new Random ();
+            var searchList = Enumerable.Range (1, 10000).Select(x => random.Next (20000)).ToArray();
+             var sw = new Stopwatch();
+            sw.Start ();
+            InsertionSort(searchList);
+            sw.Stop();
+            System.Console.WriteLine($"Sort completed. Total times = {sw.ElapsedMilliseconds} ms.");
+        }
+
+        static void InsertionSort (int[] array)
+        {
+            int inner, temp;
+            for (int outer = 1; outer <= array.Length - 1; outer++)
+            {
+                temp = array[outer];
+                inner = outer;
+                while (inner > 0 && array[inner - 1] >= temp)
+                {
+                    array[inner] = array[inner - 1];
+                    inner -= 1;
+                }
+                array[inner] = temp;
+            }
+        }
+    }
+}
---
+++ SelectionSort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Collections.Immutable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selection Sort Practice");
+            var random = new Random ();
+            var searchList = Enumerable.Range (1, 10000).Select(x => random.Next (20000)).ToArray();
+             var sw = new Stopwatch();
+            sw.Start ();
+            searchList.SafeSelectionSort();
+            sw.Stop();
+            System.Console.WriteLine($"Sort completed. Total times = {sw.ElapsedMilliseconds} ms.");
+        }
+    }
+
+    public static class SortExtensions
+    {
+        public static void SafeSelectionSort(this int[] arrays)
+        {
+            int min, temp;
+
+            for (int outer = 0; outer < arrays.Length; outer++)
+            {
+                min = outer;
+                for (int inner = outer + 1; inner < arrays.Length; inner++)
+                {
+                    if (arrays[inner] < arrays[min])
+                    {
+                        min = inner;
+                    }
+                    temp = arrays[outer];
+                    arrays[outer] = arrays[min];
+                    arrays[min] = temp;
+                }
+            }
+        }
+    }
+}
